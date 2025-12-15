64a5c90dcc37c7aeb13fc3e115bc5b92c441f516
boscomattia6 <boscomattia6@gmail.com>
2025-12-15 15:57:11 +0100

    querySamples - 1

---
+++ db/querySamples.sql
@@ -0,0 +1,7 @@
+-- 1.Programma di una manifestazione
+select m.nome, e.nome, e.OraI, e.OraF 
+from eventi e, manifestazioni m 
+where e.idManifestazione=m.id and m.nome = "[nome di una manifestazione]"
+order by e.OraI asc;
+
+-- 2.Programma di un evento
